d7dbdc07e188ee1f017a8b12363e89a04e61a1a1
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-02-21 16:36:10 +0600

    Q A section footer section responsiveness added

--- text.js
+++
@@ -1,5 +0,0 @@
-const createElements = (arr) => {
-   const htmlElement = arr.map((el) => `<span class="btn">${el}</span>`);
-};
-const synonyms = ["hello", "hi", "lakjdf"];
-createElements(synonyms);
